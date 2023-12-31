9db547aeb431b7e619a5d1188b8919ad191eeebe
medhedimaaroufi <medhedimaaroufi@gmail.com>
2023-12-31 19:05:23 +0100

    Co-authored-by: Chouchenanis <Chouchenanis@users.noreply.github.com> Co-authored-by: shayma01 <shayma01@users.noreply.github.com>

--- static/js/flip-img.js
+++ static/js/flip-img.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded',() => {
-    $('.info-resto').hide();
-    $('.resto').click(function () {
-        $('.resto').hide();
-        $('.info-resto-'+this.classList[1]).show();
+        $('.back-face').hide();
     });
-    $('.esc').click(function (){
-        $('.info-resto').hide();
-        $('.resto').show();
-    });
-});
+function flipCard(card) {
+    const cards=document.querySelectorAll('.card');
+    cards.forEach( element => {
+        if (card == element){
+            element.style.height='500px';
+            element.querySelector('.back-face').style.display='block';
+        }
+        if (card == element && element.classList.contains('flipped')){
+            card.style.height='333px';
+            element.querySelector('.back-face').style.display='none';
+        }
+        if (element != card && element.classList.contains('flipped')){
+            element.style.height='333px';
+            element.classList.toggle('flipped');
+            element.querySelector('.back-face').style.display='none';
+        }
+    })
+    card.classList.toggle('flipped');
+  }
